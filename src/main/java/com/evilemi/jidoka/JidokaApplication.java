3709c92b6e3edd102904d02abd8c6463b1c0659c
evilemi <emin.vilgenoglu@ericsson.com>
2023-12-18 22:11:22 +0300

    first commit

---
+++ src/main/java/com/evilemi/jidoka/JidokaApplication.java
@@ -0,0 +1,13 @@
+package com.evilemi.jidoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JidokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JidokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evilemi/jidoka/JidokaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evilemi.jidoka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JidokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
